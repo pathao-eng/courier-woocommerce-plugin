49e3168b7b04422c9e07afd1156cae452fb82f0f
Sagar <sagar.dash@pathao.com>
2024-04-04 16:28:19 +0600

    Update pathao-courier.php
    - update version tag

--- pathao-courier.php
+++ pathao-courier.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Pathao Courier
  * Description: Pathao Courier plugin for WooCommerce
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: Pathao
  * Text Domain: pathao-courier
  * Requires at least: 6.0
