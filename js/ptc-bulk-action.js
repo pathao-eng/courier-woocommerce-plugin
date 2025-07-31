dd0baa31a998891803b2c84aeaafa4e74cae1978
Sagar Dash <7631656+sagar290@users.noreply.github.com>
2025-08-01 01:33:40 +0600

    fix: Handsontable style

--- js/ptc-bulk-action.js
+++ js/ptc-bulk-action.js
@@ -19,17 +19,79 @@ jQuery(document).ready(function($) {
                 { data: 'order_id', readOnly: true },
                 { data: 'recipient_name', type: 'text' },
                 { data: 'recipient_phone', type: 'text' },
+                { data: 'recipient_secondary_phone', type: 'text' },
+                { data: 'recipient_address', type: 'text' },
+                { data: 'amount_to_collect', type: 'numeric' },
+                { data: 'item_description', type: 'text' },
+                { data: 'special_instruction', type: 'text' },
+                {
+                    data: 'store_id',
+                    type: 'dropdown',
+                    source: [
+                        {
+                            id: 1,
+                            name: "store 1"
+                        }
+                    ],
+                    optionLabel: 'name',
+                    value: 'id',
+                    allowInvalid: true,
+                    strict: true,
+                },
                 {
                     data: 'delivery_type',
                     type: 'dropdown',
-                    source: ['48', '49'],
-                    allowInvalid: false,
+                    source: [
+                        {
+                            id: 48,
+                            name: "normal"
+                        }
+                    ],
+                    optionLabel: 'name',
+                    value: 'id',
+                    allowInvalid: true,
+                    strict: true,
                 },
-                { data: 'amount_to_collect', type: 'numeric' }
+                {
+                    data: 'item_type',
+                    type: 'dropdown',
+                    source: [
+                        {
+                            id: 2,
+                            name: "parcel"
+                        },
+                        {
+                            id: 3,
+                            name: "document"
+                        }
+                    ],
+                    optionLabel: 'name',
+                    value: 'id',
+                    allowInvalid: true,
+                    strict: true,
+                },
+                { data: 'item_quantity', type: 'numeric' },
+                { data: 'item_weight', type: 'numeric' },
             ],
-            colHeaders: ['Order ID', 'Recipient Name', 'Recipient Phone', 'Delivery Type', 'Amount to Collect'],
+            colHeaders: [
+                'Order ID',
+                'Recipient Name',
+                'Recipient Phone',
+                'Recipient Secondary Phone',
+                'Address',
+                'Collectable Amount',
+                'Note',
+                'Special Instruction',
+                'Store',
+                'Delivery Type',
+                'Item Type',
+                'Quantity',
+                'Weight',
+            ],
             rowHeaders: true,
             width: '100%',
+            height: 300,
+            stretchH: 'all',
             licenseKey: 'non-commercial-and-evaluation'
         });
     }
@@ -39,7 +101,7 @@ jQuery(document).ready(function($) {
     $('body').append(`
         <div id="custom-modal-overlay" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%;
             background: rgba(0,0,0,0.5); z-index: 10000;">
-             <div style="padding: 50px; margin: 0 auto; max-width: 960px; background: white">
+             <div class="ptc-modal-bulk-order">
                 <h2 class="wp-heading-inline ">Send with Pathao</h2>
                 <div id="hot-container" style="margin: 50px auto; max-width:960px;"></div>
             </div>
